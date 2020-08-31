d22a44180ded964307b48ba520cfbabca970ab9d
Denis Korobov <dkorobov@devexperts.com>
2020-08-31 12:22:26 +0300

    implemented pieCase

--- src/main/kotlin/com/devexperts/pie/Diner.kt
+++ src/main/kotlin/com/devexperts/pie/Diner.kt
@@ -12,7 +12,11 @@ import akka.japi.pf.ReceiveBuilder
  */
 fun main(args: Array<String>) {
 
-    class CustomerActor (val waiter: ActorRef) : AbstractLoggingActor() {
+    data class GetSlice(val customer: ActorRef)
+    data class AddToOrder(val customer: ActorRef)
+
+    class CustomerActor(val waiter: ActorRef) : AbstractLoggingActor() {
+
         override fun createReceive() =
                 ReceiveBuilder()
                         .match(String::class.java, this::onMessage)
@@ -21,32 +25,56 @@ fun main(args: Array<String>) {
         private fun onMessage(message: String) {
             when (message) {
                 "hungry for pie" -> waiter.tell("order", self())
-                "put on table" -> throw NotImplementedError()
-                "no pie left" -> throw NotImplementedError()
+                "put on table" -> log().info("tasty pie")
+                "no pie left" -> log().info("sad story")
                 else -> log().error("Unknown message: $message")
             }
         }
     }
 
-    class WaiterActor : AbstractLoggingActor() {
+    class WaiterActor(val pieCase: ActorRef) : AbstractLoggingActor() {
+
         override fun createReceive() =
                 ReceiveBuilder()
                         .match(String::class.java, this::onMessage)
+                        .match(AddToOrder::class.java, this::addToOrder)
                         .build()
 
         private fun onMessage(message: String) {
             when (message) {
-                "order" -> log().info("Don't know how to order order from ${sender.path().name()}")
-                "add to order" -> throw NotImplementedError()
-                "error" -> throw NotImplementedError()
+                "order" -> pieCase.tell(GetSlice(sender), self)
                 else -> log().error("Unknown message: $message")
             }
+        }
+
+        private fun addToOrder(addToOrder: AddToOrder) {
+            addToOrder.customer.tell("put on table", self)
+        }
+    }
+
+    class PieCaseActor(var slices: MutableList<String>) : AbstractLoggingActor() {
+
+        override fun createReceive() =
+                ReceiveBuilder()
+                        .match(GetSlice::class.java, this::onGetSlice)
+                        .build()
+
+        private fun onGetSlice(getSlice: GetSlice) {
+            if (slices.size <= 0) {
+                        log().error("no pie left")
+                        getSender().tell("error", self)
+                    } else {
+                        val slice = slices.removeFirst()
+                        log().info("Slice: $slice")
+                        getSender().tell(AddToOrder(getSlice.customer), self)
+                    }
         }
     }
 
     val actorSystem = ActorSystem.create("akka-diner")
 
-    val waiter = actorSystem.actorOf(Props.create(WaiterActor::class.java), "waiter")
+    val pieCase = actorSystem.actorOf(Props.create(PieCaseActor::class.java, mutableListOf<String>("apple", "peach", "cherry")), "pieCase")
+    val waiter = actorSystem.actorOf(Props.create(WaiterActor::class.java, pieCase), "waiter")
     val customer1 = actorSystem.actorOf(Props.create(CustomerActor::class.java, waiter), "customer1")
     val customer2 = actorSystem.actorOf(Props.create(CustomerActor::class.java, waiter), "customer2")
 
